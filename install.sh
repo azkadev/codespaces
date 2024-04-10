112ed1f89fe90b5768c784d247d3ff9487bff885
AZKA <82513502+azkadev@users.noreply.github.com>
2024-04-10 02:31:41 +0000

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 
 sudo apt update -y
 sudo apt-get install apt-transport-https -y 
@@ -6,10 +6,7 @@ mkdir -p $HOME/development
 cd $HOME/development
 git clone --depth 1 --branch stable https://github.com/flutter/flutter
 echo "export PATH=$PATH:$HOME/development/flutter/bin" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/development/flutter/bin" >> $HOME/.zshrc
 source $HOME/.bashrc
 flutter doctor
-
-# sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-# sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-# sudo apt update -y
-# sudo apt-get install dart -y
+ 
