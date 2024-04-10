100db65431ae151338d334564a1b08429e495b50
AZKA <82513502+azkadev@users.noreply.github.com>
2024-04-10 02:32:14 +0000

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 sudo apt update -y
 sudo apt-get install apt-transport-https -y 
-mkdir -p $HOME/development
-cd $HOME/development
+mkdir -p "$HOME/development"
+cd "$HOME/development"
 git clone --depth 1 --branch stable https://github.com/flutter/flutter
 echo "export PATH=$PATH:$HOME/development/flutter/bin" >> $HOME/.bashrc
 echo "export PATH=$PATH:$HOME/development/flutter/bin" >> $HOME/.zshrc
