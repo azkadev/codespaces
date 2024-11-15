a069f3c89cd52cdaea14f972795ca0ca455ac73a
AZKA <82513502+azkadev@users.noreply.github.com>
2024-11-15 13:07:34 +0000

    update

--- programs/mega/mega-cmd-ubuntu-20.04.sh
+++ programs/mega/mega-cmd-ubuntu-20.04.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb && sudo apt install "$PWD/megacmd-xUbuntu_20.04_amd64.deb"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb && sudo apt install "$PWD/megacmd-xUbuntu_20.04_amd64.deb" -y
